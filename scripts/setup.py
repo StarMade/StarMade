ecc63c14c9467bfc50b1514ad55947c9233a292c
Raul Tambre <raul.tambre@gmail.com>
2013-11-24 22:30:33 +0200

    fixes, dynamic downloads and MUCH MORE!
    Really...Removed all deobfuscating, decompiling, changed some stuff,
    improved in small portions perfomance, now downloads a new StarMade
    build when you don't have one, fixed couple small problem, removed a
    huge portion of md5 code and well couple other things.

--- scripts/setup.py
+++ scripts/setup.py
@@ -26,26 +26,6 @@ def getVersion(line):
 	ver = line.split('=', 1)[1]
 	return ver
 
-def getMD5(file, block_size=2**20):
-        import hashlib
-        md5 = hashlib.md5()
-        while True:
-                data = file.read(block_size)
-                if not data:
-                        break
-                md5.update(data)
-        return md5.digest()
-
-def sumfile(fobj):
-    '''Returns an md5 hash for an object with read() method.'''
-    m = md5.new()
-    while True:
-        d = fobj.read(8096)
-        if not d:
-            break
-        m.update(d)
-    return m.hexdigest()
-
 def main(argv):
         ignoreupdates = False
         hasfailed = False
@@ -62,61 +42,67 @@ def main(argv):
         print ('--------- Welcome to SMCP v%s ---------' % getVersion(0))
         print ('-----------------------------------------\n')
         workingDir = os.getcwd()
+        smbuild = getVersion(2)
+        smver = getVersion(1)
         if ignoreupdates == False:
-                print ('Checking for updates...')
-                try:
-                        #import hashlib
-                        starmademdweb = urllib.request.urlopen("http://smcp.pingu.pw/file.md5");
-                        starmademdraw = starmademdweb.read()
-                        #print (sumfile(open(workingDir + '\install\StarMade.zip')))
-                except urllib.error.HTTPError as exception:
-                        print ('    *   Unable to get latest version info - HTTPError =  ' + str(exception.reason))
-                except urllib.error.URLError as exception:
-                        print ('    *   Unable to get latest version info - URLError = ' + str(exception.reason))
-                        sys.exit(2)
-                except urllib.error.HTTPException as exception:
-                        print ('    *   Unable to get latest version info - HTTPException')
-                        sys.exit(2)
-                except Exception as exception:
-                        import traceback
-                        print ('    *   Unable to get latest version info - Exception = ' + traceback.format_exc())
-                        sys.exit(2)
-                print ('*   Checking StarMade for an update...')
-                starmademdweb.close()
-                print (starmademdraw)
-        print ('Extracting StarMade v%s\n' % getVersion(1))
+                #try:
+                #        #import hashlib
+                #        starmademdweb = urllib.request.urlopen("http://smcp.pingu.pw/file.md5");
+                #        starmademdraw = starmademdweb.read()
+                #        #print (sumfile(open(workingDir + '\install\StarMade.zip')))
+                #except urllib.error.HTTPError as exception:
+                #        print ('    *   Unable to get latest version info - HTTPError =  ' + str(exception.reason))
+                #except urllib.error.URLError as exception:
+                #        print ('    *   Unable to get latest version info - URLError = ' + str(exception.reason))
+                #        sys.exit(2)
+                #except urllib.error.HTTPException as exception:
+                #        print ('    *   Unable to get latest version info - HTTPException')
+                #       sys.exit(2)
+                #except Exception as exception:
+                #        import traceback
+                #        print ('    *   Unable to get latest version info - Exception = ' + traceback.format_exc())
+                #        sys.exit(2)
+                #starmademdweb.close()
+                #print (starmademdraw)
+                print ('Checking for updates... (DISABLED)')
+                if not os.path.isfile(workingDir + '\install\starmade-build_' + smbuild + '.zip'):
+                        print ('*   Getting new StarMade... (May take a while)\n')
+                        url = r'http://files.star-made.org/build/starmade-build_' + smbuild + '.zip'
+                        opener = urllib.request.urlopen(url)
+                        starmade_out = open(workingDir + '\install\starmade-build_' + smbuild + '.zip', 'wb')
+                        starmade_out.write(opener.readall())
+                        starmade_out.close()
+                else:
+                        print ('\n')
+        print ('Extracting StarMade v' + smver + '\n')
         if not os.path.exists('instance') and not os.path.isdir('instance'):
                 os.makedirs('instance')
         if not os.path.exists('tmp') and not os.path.isdir('tmp'):
                 os.makedirs('tmp')
         if not os.path.exists('conf') and not os.path.isdir('conf'):
                 os.makedirs('conf')
-        os.chdir(workingDir + '\install')
-        unzip("StarMade.zip", workingDir + '\instance')
-        os.chdir(workingDir)
+        unzip(workingDir + '\install\starmade-build_' + smbuild + '.zip', workingDir + '\instance')
         print ('Decompiling StarMade')
-        print ('*   Deobfuscating... (Stage #1)')
-        print ('*       Not here yet, skipping')
+        print ('*   Deobfuscating... (Stage #1) (DISABLED)')
+        #print ('*       Not here yet, skipping')
         #startProcess("java -Xmx1G -jar runtime/N3Remapper.jar conf/remapper.cfg pre instance/StarMade.jar tmp/deobf.zip")
-        print ('*   Decompiling...   (Stage #2)')
+        print ('*   Decompiling...   (Stage #2) (DISABLED)')
         if not os.path.exists('sources') and not os.path.isdir('sources'):
                 os.makedirs('sources')
         #tmp/deobf.zip when we have implemented SpecialSource
-        startProcess("java -Xmx1G -jar runtime/fernflower.jar install/StarMade.zip sources")
-        os.chdir(workingDir + '\install')
+        #startProcess('java -Xmx1G -jar runtime/fernflower.jar' + workingDir + 'install/StarMade.zip sources')
         print ('Setting up Eclipse workspace\n')
-        unzip("EclipseWorkspace.zip", workingDir)
-        os.chdir(workingDir)
-        if os.path.exists('tmp'):
+        unzip(workingDir + "\install\EclipseWorkspace.zip", workingDir)
+        if os.path.exists(workingDir + 'tmp'):
                 print ('Deleting temporary files\n')
-                shutil.rmtree('tmp')
+                shutil.rmtree(workingDir + 'tmp')
         endMessage(hasfailed)
 
 def endMessage(failed):
         if failed:
-                print ('---------------------')
-                print ('- SMCP setup failed -')
-                print ('---------------------')
+                print ('-----------------------------------------')
+                print ('----------- SMCP setup failed -----------')
+                print ('-----------------------------------------')
         else:
                 print ('-----------------------------------------')
                 print ('- SMCP Is now ready for mod development -')
